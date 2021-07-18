e711bc1a1a1dccdf03ec3c60110126ddf04adb5b
Juan@SDSharp <juan@sdsharp.com>
2021-07-18 10:29:08 -0500

    Dealing Damage

--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -7,6 +7,7 @@ public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] Rigidbody rb = null;
     [SerializeField] float launchForce = 10f;
+    [SerializeField] int damageToDeal = 20;
     [SerializeField] float destroyAfterSeconds = 5f;
 
     private void Start()
@@ -17,6 +18,22 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) return;
+        }
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
     }
 
     [Server]
