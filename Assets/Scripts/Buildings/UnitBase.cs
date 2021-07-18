1334c0ae6971fe8c615b5eb4e5a657aa9c2499b2
Juan@SDSharp <juan@sdsharp.com>
2021-07-18 15:42:31 -0500

    Unit Base Building

---
+++ Assets/Scripts/Buildings/UnitBase.cs
@@ -0,0 +1,38 @@
+using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBase : NetworkBehaviour
+{
+    [SerializeField] Health health = null;
+
+    public static event Action<UnitBase> ServerOnBaseSpawned;
+    public static event Action<UnitBase> ServerOnBaseDespawned;
+
+    #region Server
+    public override void OnStartServer()
+    {
+        health.ServerOnDie += ServerHandleDie;
+        ServerOnBaseSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnBaseDespawned?.Invoke(this);
+        health.ServerOnDie -= ServerHandleDie;
+    }
+
+    [Server] 
+    private void ServerHandleDie()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+
+    #endregion
+
+    #region Client
+
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -2,10 +2,13 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] GameObject _unitSpawnerPrefab;
+    [SerializeField] GameOverHandler gameOverHandlerPrefab;
+
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
@@ -13,5 +16,15 @@ public class RTSNetworkManager : NetworkManager
         GameObject unitSpawnerInstance = Instantiate(_unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
 
         NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
+
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        if (SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
     }
 }
